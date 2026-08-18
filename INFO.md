7624e9ae5ff0998d10feb3c21244c296ba15342f
johndoe <johndoe@email.com>
2026-08-18 10:35:58 +0200

    02 HTML Frontend for Your API - Jinja2 Templates

--- INFO.md
+++ INFO.md
@@ -2,7 +2,7 @@
 [https://www.youtube.com/playlist?list=PL-osiE80TeTsak-c-QsVeg0YYG_0TeyXI]
 
 ## 01 Getting Started - Web App + REST API
-[https://www.youtube.com/watch?v=7AMjmCTumuo&list=PL-osiE80TeTsak-c-QsVeg0YYG_0TeyXI&index=2]
+[https://www.youtube.com/watch?v=7AMjmCTumuo&list=PL-osiE80TeTsak-c-QsVeg0YYG_0TeyXI&index=1]
 [https://github.com/CoreyMSchafer/FastAPI-01-Getting-Started]
 
 ``` bash
@@ -19,4 +19,15 @@ http://localhost:8000/
 http://localhost:8000/docs
 http://localhost:8000/redoc
 ```
+
+## 02 HTML Frontend for Your API - Jinja2 Templates
+[https://www.youtube.com/watch?v=G4NIB9Rx9Qs&list=PL-osiE80TeTsak-c-QsVeg0YYG_0TeyXI&index=2]
+[https://github.com/CoreyMSchafer/FastAPI-02-Templates]
+
+```bash
+# Start webserver
+uv run fastapi dev main.py
+```
+
+Current: 13:12
 
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
@@ -21,12 +26,16 @@ posts: list[dict] = [
 ]
 
 
-@app.get(path="/", response_class=HTMLResponse, include_in_schema=False)
-@app.get(path="/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+@app.get(path="/", include_in_schema=False, name="home")
+@app.get(path="/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"posts": posts, "title": "Home"},
+    )
+
 
 @app.get("/api/posts")
 def get_posts():
     return posts
-
